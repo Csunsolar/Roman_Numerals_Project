5111c61194f4656fb61e9468158641c1ca0229c6
Csunsolar <christophersun09@gmail.com>
2024-02-03 00:25:02 -0600

    can now add first instance of a substraction but need to continue looking through rest of input in case there is another instance that subtraction is needed.

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,9 +1,10 @@
 rndict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
 r_numerals = input("Type in Roman Numerals Here: ")
+addition = ""
 
-
-if 1 < len(r_numerals) <= 4:
+#check to make sure that length of roman numeral is valid
+if 1 < len(r_numerals) <= 7:
     x, y = [0, 1]
     for q in range(len(r_numerals)-1):
         enumerate(r_numerals)
@@ -13,19 +14,28 @@ if 1 < len(r_numerals) <= 4:
             y += 1
         else:
             addition = False
-elif len(r_numerals) > 4:
-    print("invalid")
+            #gets the difference between the smaller roman numeral and the larger roman numeral
+            z = rndict.get(r_numerals[x+1]) - rndict.get(r_numerals[x])
+            x += 1
+            y += 1
+            for q in range(len(r_numerals)-1):
+                if rndict.get(r_numerals[x]) >= rndict.get(r_numerals[y]):
+                    x += 1
+                    y += 1            
+elif len(r_numerals) > 7:
+    print("Invalid. Roman Numerals can only be 7 characters long")
 else:    
     addition = True
 
-if addition == True:
-    r_list = []
-    for a in r_numerals:
-       r_list.append(rndict.get(a))
-    print(sum(r_list))
+#makes list of dict values for each character of r_numerals
+r_list = []
+for a in r_numerals:
+   r_list.append(rndict.get(a))
 
-print(addition)
-#printing bool
+if addition == True:
+    print(sum(r_list))
+elif addition == False:
+    print(sum(r_list[:x]) + z)
 
 
 
